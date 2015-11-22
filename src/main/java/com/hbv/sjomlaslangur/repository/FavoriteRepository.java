d9460e08da3b5a318d43e38d170bb64f1b8db86b
Arnar Þór <arnargisla@gmail.com>
2015-11-22 02:14:32 +0000

    Adds ability to unfavorite a phrase

--- src/main/java/com/hbv/sjomlaslangur/repository/FavoriteRepository.java
+++ src/main/java/com/hbv/sjomlaslangur/repository/FavoriteRepository.java
@@ -3,6 +3,7 @@ package com.hbv.sjomlaslangur.repository;
 import com.hbv.sjomlaslangur.domain.Favorite;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +14,9 @@ public interface FavoriteRepository extends JpaRepository<Favorite,Long> {
 
     @Query("select favorite from Favorite favorite where favorite.userthatfavorited.login = ?#{principal.username}")
     List<Favorite> findByUserthatfavoritedIsCurrentUser();
+
+    @Query("select favorite from Favorite favorite where favorite.userid = :userid and favorite.phraseid = :phraseid ")
+    List<Favorite> findSpecific(@Param("userid") Long userid, @Param("phraseid")Long phraseid);
+
 
 }
